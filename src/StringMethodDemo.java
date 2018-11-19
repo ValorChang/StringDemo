c1d1a0e84b545c96dce81aab1bed7f700732b3ad
ValorChang <chandi92@mail.com>
2018-11-20 00:17:08 +0800

    2018/11/20 master branches

--- src/StringMethodDemo.java
+++ src/StringMethodDemo.java
@@ -1,7 +1,6 @@
 
 public class StringMethodDemo {
 	public static void main(String[] arges){
-		System.out.println("mac test");
-		System.out.println("home test");
+		System.out.println("StringDemo");
 	}
 }
