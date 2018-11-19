78c5a31ebd6c59c2c1ca7c2ec677e446daf3687a
ValorChang <chandi92@mail.com>
2018-11-20 00:11:36 +0800

    2018/11/20 mac branches

--- src/StringMethodDemo.java
+++ src/StringMethodDemo.java
@@ -3,5 +3,6 @@ public class StringMethodDemo {
 	public static void main(String[] arges){
 		System.out.println("mac test");
 		System.out.println("home test");
+        System.out.println("mac test2");
 	}
 }
