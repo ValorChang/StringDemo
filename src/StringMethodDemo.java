d7ab3ea52e86603b4004e54e9d506b5d5e03d400
valorchang <changdi92@gmail.com>
2018-12-03 00:07:23 +0800

    2018/12/03 commit

--- src/StringMethodDemo.java
+++ src/StringMethodDemo.java
@@ -1,8 +1,9 @@
 
 public class StringMethodDemo {
-	public static void main(String[] arges){
-		System.out.println("mac test");
+	public static void main(String[] arges) {
+        System.out.println("mac test");
         System.out.println("2018/11/22");
         System.out.println("2018/12/03");
-	}
+        System.out.println("git");
+    }
 }
